fc2163b59982b2d47b361802470d8f5c5ed9a0ca
Nayamode <Nayamode@Nayamodes-iMac.local>
2012-12-07 11:47:53 +0530

    Initial Commit

---
+++ Hello_Empty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello_Empty
+//
+//  Created by Nayamode on 07/12/12.
+//  Copyright (c) 2012 Nayamode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello_Empty/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello_Empty
+//
+//  Created by Nayamode on 07/12/12.
+//  Copyright (c) 2012 Nayamode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
